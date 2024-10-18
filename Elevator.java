f41c64548d5f86004b91da452208b3fc13987ecc
Bilberry04 <kacper.borowiec04@gmail.com>
2024-10-18 12:55:40 +0200

    Second step: adding random people to elevator and random floors

--- Elevator.java
+++ Elevator.java
@@ -1,29 +1,42 @@
 import java.util.Collections;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Elevator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("|Witaj w algorytmie windy \n|Winda znajduje się w bloku 9 piętrowym \n|Podaj 4 piętra, które w jednym momencie mają zostać obsłużone przez windę");
-        System.out.println("|Wprowadź dane: ");
-        System.out.print("|Piętro a: ");
-        int a = sc.nextInt();
-        System.out.print("|Piętro b: ");
-        int b = sc.nextInt();
-        System.out.print("|Piętro c: ");
-        int c = sc.nextInt();
-        System.out.print("|Piętro d: ");
-        int d = sc.nextInt();
+        Random rand = new Random();
 
-        Integer[] floor = {a, b, c, d};
-        Arrays.sort(floor, Collections.reverseOrder());
+        System.out.println("|Witaj w algorytmie windy \n|Winda znajduje się w bloku 9 piętrowym \n|Podaj piętro, z którego chcesz wsiąść oraz piętro docelowe");
+        System.out.println("|Wprowadź dane: ");
+        System.out.print("|Wsiadam z: ");
+        int from1 = sc.nextInt();
+        System.out.print("|Wysiadam na: ");
+        int to1 = sc.nextInt();
+
+        // Tworzymy tablice dla losowych pięter, zaczynając od `from1` i `to1`
+        Integer[] floorFrom = new Integer[4];
+        Integer[] floorTo = new Integer[4];
+
+        // Pierwsze piętro to te podane przez użytkownika
+        floorFrom[0] = from1;
+        floorTo[0] = to1;
+
+        // Generowanie losowych pięter
+        for (int i = 1; i < 4; i++) {
+            floorFrom[i] = rand.nextInt(10); // Losowe piętra od 0 do 9
+            floorTo[i] = rand.nextInt(10);
+        }
+
+        // Sortowanie w kolejności malejącej
+        Arrays.sort(floorFrom, Collections.reverseOrder());
 
         System.out.println("Kolejność obsługi pięter (od najwyższego do najniższego):");
-        for (int i = 0; i < floor.length; i++) {
-            System.out.print(floor[i]);
-            if(i < floor.length - 1) {
-                System.out.print(">");
+        for (int i = 0; i < floorFrom.length; i++) {
+            System.out.print(floorFrom[i]);
+            if (i < floorFrom.length - 1) {
+                System.out.print(" > ");
             }
         }
 
